b79787cd1852558c7f6e47211f2a17b0c4af087a
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:15:24 +0200

    Add project files.

---
+++ SupermercatoSconti/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SupermercatoSconti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inserisci l'importo che hai speso: ");
+            double spesa = double.Parse(Console.ReadLine());
+            if (spesa > 100)
+            {
+                double sconto = (spesa / 100) * 20;
+                double spesascont = spesa - sconto;
+                Console.WriteLine($"Hai diritto allo sconto del 20%, il totale della spesa è quindi {spesascont} euro");
+
+            }
+            else Console.WriteLine($"Mi dispiace non hai diritto allo sconto, Il totale della spesa rimane {spesa} euro");
+            Console.ReadLine();
+
+        }
+    }
+}
